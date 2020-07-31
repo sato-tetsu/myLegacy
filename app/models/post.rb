f0783551f864ea576f44d24a600412d10355b26f
vagrant <vagrant@localhost.localdomain>
2020-07-31 14:00:32 +0000

    add like_comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ApplicationRecord
 	belongs_to :user
     has_many :comments, dependent: :destroy
+	has_many :likes, dependent: :destroy
+
+	def liked_by?(user)
+	    likes.where(user_id: user.id).exists?
+	end
+
 end
