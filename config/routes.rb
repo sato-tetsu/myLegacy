2b5a01b7cf764af4f38f87633512c97c48477d71
vagrant <vagrant@localhost.localdomain>
2020-08-06 13:37:41 +0000

    message

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 	resources :users
 	resources :posts do
 		resource :likes, only: [:create, :destroy]
-		resources :favorites, only: [:create, :destroy]
+		resource :favorites, only: [:create, :destroy]
 		resources :comments, only: [:create, :destroy]
 	end
 
