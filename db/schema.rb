ae3f510c0dd1b5d27a21bbb893be5793d8ca1e77
vagrant <vagrant@localhost.localdomain>
2020-08-12 12:04:49 +0000

    mod-migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_10_084049) do
+ActiveRecord::Schema.define(version: 2020_08_12_120214) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -44,10 +44,10 @@ ActiveRecord::Schema.define(version: 2020_08_10_084049) do
   end
 
   create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
     t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "follower_id"
   end
 
   create_table "users", force: :cascade do |t|
